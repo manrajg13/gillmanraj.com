3bccd2ad21a0f25768696d875110d5ce2e1b6931
Manraj <manraj836@gmail.com>
2022-09-16 03:11:19 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
 	})
 
 	return (
-		<nav id='n' className='fixed ml-[-375px] py-4 left-[50%] top-0 text-sm font-bold text-white w-[750px] bg-background/[0.8] z-14 transition -translate-y-12 delay-[3000ms] duration-500'>
+		<nav id='n' className='fixed ml-[-375px] py-4 left-[50%] top-0 z-10 text-sm font-bold text-white w-[750px] bg-background/[0.8] z-14 transition -translate-y-12 delay-[3000ms] duration-500'>
 			<div className='float-left'>
 				<Link href='/'>
 					<span className='p-3 hover:cursor-pointer hover:text-red transition duration-150 hover:ease-in'>MANRAJ GILL</span>
